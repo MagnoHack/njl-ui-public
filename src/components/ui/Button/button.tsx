fbd964938f758dd96d315558aa59fb50d8d019e6
GersonEC <gersonenriquez94@gmail.com>
2025-03-14 12:42:59 +0100

    feat: add button colors variables

--- src/components/ui/Button/button.tsx
+++ src/components/ui/Button/button.tsx
@@ -10,15 +10,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+          'bg-primary-btn text-primary-btn-foreground shadow-xs hover:bg-primary-btn/80',
         destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+          'bg-destructive text-white shadow-xs hover:bg-destructive/80 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline_primary:
-          'border border-primary text-primary bg-background shadow-xs hover:text-primary/90 dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'border border-primary-btn-outline text-primary-btn-outline-foreground shadow-xs hover:border-primary-btn-outline/80 hover:text-primary-btn-outline-foreground/80',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-md hover:bg-secondary/80',
+          'bg-secondary-btn text-secondary-btn-foreground shadow-md hover:bg-secondary-btn/80',
         outline_secondary:
-          'border border-white text-white bg-transpa shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'border border-secondary-btn-outline text-secondary-btn-outline-foreground shadow-xs hover:border-secondary-btn-outline/80 hover:text-secondary-btn-outline-foreground/80',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
